ffb837df1399a374449e3a84ce2747b9c18870e2
jonathan-whitehat <76190639+jonathan-whitehat@users.noreply.github.com>
2021-09-21 17:03:03 -0400

    Add files via upload

---
+++ DESK.js
@@ -0,0 +1,22 @@
+img = "";
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+
+function preload()
+{
+    img = loadImage('DESK.jpg');
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
+
+function back()
+{
+    window.location = ("index.html");
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function buttonBR()
+{
+    window.location=("BR.html")
+}
+
+function buttonBOT()
+{
+    window.location=("BOT.html")
+}
+
+function buttonDESK()
+{
+    window.location=("DESK.html")
+}
+
+function buttonPLNT()
+{
+    window.location=("PLNT.html")
+}
+
+function buttonTV()
+{
+    window.location=("TV.html")
+}
